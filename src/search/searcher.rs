99f398cb896235b00ef19babdf810f2513a34f61
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-06 10:04:53 +0200

    finish `search` implementation

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -1,4 +1,4 @@
-use crate::{error::Result, utils::Jsonify, EbayApiClient};
+use crate::{error::Result, models::search::SearchResults, utils::Jsonify, EbayApiClient};
 use reqwest::Method;
 
 const ENDPOINT: &str = "buy/browse/v1/item_summary/search";
@@ -6,16 +6,16 @@ const ENDPOINT: &str = "buy/browse/v1/item_summary/search";
 pub struct Searcher<'c>(pub(crate) &'c EbayApiClient);
 
 impl<'c> Searcher<'c> {
-    pub fn search<S: AsRef<str>>(&self, query: S, limit: u16) -> Result<()> {
+    pub fn search<S: AsRef<str>>(&self, query: S, limit: u16) -> Result<SearchResults> {
         let limit_as_str = limit.to_string();
         let query = ["q", query.as_ref(), "limit", limit_as_str.as_str()];
 
         let builder = self.0.request_builder(Method::GET, ENDPOINT).query(&query);
         let response = builder.send()?;
 
-        response.error_for_status()?;
-        //let response = response.jsonify();
+        let response = response.error_for_status()?;
+        let response = response.jsonify::<SearchResults>()?;
 
-        todo!()
+        Ok(response)
     }
 }
