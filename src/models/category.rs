67c5ee11f251c56de29aa76e7cb66a167597509a
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-06 12:39:42 +0200

    make category id a `u32`

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,10 +1,14 @@
 use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
 
 /// A category.
+#[serde_as]
 #[derive(Debug, Clone, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Category {
+    #[serde_as(as = "DisplayFromStr")]
     #[serde(rename = "categoryId")]
-    id: Box<str>,
+    id: u32,
+
     #[serde(rename = "categoryName")]
     name: Box<str>,
 }
