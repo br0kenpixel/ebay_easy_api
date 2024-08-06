6c5803654fb1c85a28e0b4d2a37fe456daebd385
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-06 12:48:36 +0200

    refactor: `Seller`

--- src/models/seller.rs
+++ src/models/seller.rs
@@ -1,16 +1,27 @@
+use rust_decimal::Decimal;
 use serde::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
+use std::fmt::Display;
 
 /// An Ebay seller.
 #[serde_as]
 #[derive(Debug, Deserialize, Clone, PartialEq, PartialOrd)]
 pub struct Seller {
-    username: Box<str>,
+    /// Username of the seller account.
+    pub username: Box<str>,
 
+    /// Feedback percentage as a fixed-precision decimal number.
     #[serde_as(as = "DisplayFromStr")]
     #[serde(rename = "feedbackPercentage")]
-    feedback_percentage: f32,
+    pub feedback_percentage: Decimal,
 
+    /// Feedback score.
     #[serde(rename = "feedbackScore")]
-    feedback_score: u32,
+    pub feedback_score: u32,
+}
+
+impl Display for Seller {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.username)
+    }
 }
