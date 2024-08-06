66213418924394f52962292b706dc7445c4361ba
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-06 11:08:52 +0200

    fix incorrect type and change `feedback_percentage` parser

--- src/models/seller.rs
+++ src/models/seller.rs
@@ -1,10 +1,15 @@
 use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
 
+#[serde_as]
 #[derive(Debug, Deserialize, Clone, PartialEq, PartialOrd)]
 pub struct Seller {
     username: Box<str>,
+
+    #[serde_as(as = "DisplayFromStr")]
     #[serde(rename = "feedbackPercentage")]
     feedback_percentage: f32,
+
     #[serde(rename = "feedbackScore")]
     feedback_score: u32,
 }
