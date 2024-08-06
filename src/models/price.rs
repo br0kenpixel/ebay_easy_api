9740fcf86007f19e06ef87a5ae131248d5fb2592
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-06 11:18:15 +0200

    refactor: serialize price currency and value as fixed-point decimal

--- src/models/price.rs
+++ src/models/price.rs
@@ -1,7 +1,21 @@
+use rust_decimal::Decimal;
 use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
 
+#[serde_as]
 #[derive(Debug, Deserialize, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Price {
-    value: Box<str>,
-    currency: Box<str>,
+    #[serde_as(as = "DisplayFromStr")]
+    value: Decimal,
+    currency: Currency,
+}
+
+#[derive(Debug, Deserialize, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Currency {
+    #[serde(rename = "EUR")]
+    Euro,
+    #[serde(rename = "USD")]
+    UsDollar,
+    #[serde(rename = "HUF")]
+    Forint,
 }
