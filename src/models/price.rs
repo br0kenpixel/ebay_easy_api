138bae90dbd2988862b001a56fc708c0bcbeb271
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-06 12:46:06 +0200

    add `Display` implementation to `Price`

--- src/models/price.rs
+++ src/models/price.rs
@@ -1,6 +1,8 @@
+use derive_more::Display;
 use rust_decimal::Decimal;
 use serde::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
+use std::{fmt::Display, ops::Deref};
 
 /// A fixed-precision decimal number representation of a price value.
 #[serde_as]
@@ -12,12 +14,29 @@ pub struct Price {
 }
 
 /// A currency type.
-#[derive(Debug, Deserialize, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Deserialize, Display, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Currency {
+    #[display(fmt = "€")]
     #[serde(rename = "EUR")]
     Euro,
+    #[display(fmt = "$")]
     #[serde(rename = "USD")]
     UsDollar,
+    #[display(fmt = "HUF")]
     #[serde(rename = "HUF")]
     Forint,
+}
+
+impl Display for Price {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.value, self.currency)
+    }
+}
+
+impl Deref for Price {
+    type Target = Decimal;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
 }
