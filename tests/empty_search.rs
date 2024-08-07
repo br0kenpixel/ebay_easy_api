f6c05eaef97ab1fc4bd8609e3ce27ae090fe27d7
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2024-08-07 08:07:35 +0200

    add test for empty search

---
+++ tests/empty_search.rs
@@ -0,0 +1,18 @@
+use ebay_easy_api::{models::Marketplace, EbayApiClient};
+use std::env::var;
+
+#[test]
+fn empty_search() {
+    let client =
+        EbayApiClient::new_unchecked(var("EBAY_TOKEN").unwrap(), Marketplace::UnitedStates);
+
+    let searcher = client.search();
+    let results = searcher
+        .search("kjefbasrfbsifebfoubefiuwbfwuf", 10)
+        .unwrap();
+
+    assert!(results.items.is_empty());
+    assert_eq!(results.total, 0);
+    assert_eq!(results.len(), 0);
+    assert!(results.is_empty());
+}
